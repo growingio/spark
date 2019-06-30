26514df1684e78b69d79629b55e4291da7e072e9
IceMimosa <chk19940609@gmail.com>
2019-07-01 00:58:07 +0800

    [AE2.3-06] Collect row count statistics for each partition

--- core/src/main/java/org/apache/spark/shuffle/sort/SpillInfo.java
+++ core/src/main/java/org/apache/spark/shuffle/sort/SpillInfo.java
@@ -17,20 +17,22 @@
 
 package org.apache.spark.shuffle.sort;
 
-import java.io.File;
-
 import org.apache.spark.storage.TempShuffleBlockId;
+
+import java.io.File;
 
 /**
  * Metadata for a block of data written by {@link ShuffleExternalSorter}.
  */
 final class SpillInfo {
   final long[] partitionLengths;
+  final long[] partitionRecords;
   final File file;
   final TempShuffleBlockId blockId;
 
   SpillInfo(int numPartitions, File file, TempShuffleBlockId blockId) {
     this.partitionLengths = new long[numPartitions];
+    this.partitionRecords = new long[numPartitions];
     this.file = file;
     this.blockId = blockId;
   }
