29fbc326aca58bd2993a5b856f1fc3897e53456c
IceMimosa <chk19940609@gmail.com>
2019-11-04 11:40:57 +0800

    Fix code style

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
@@ -189,14 +189,17 @@ object SharedState extends Logging {
   }
 
   private val HIVE_EXTERNAL_CATALOG_CLASS_NAME = "org.apache.spark.sql.hive.HiveExternalCatalog"
-  private val HIVE_EXTERNAL_MEMORY_CATALOG_CLASS_NAME = "org.apache.spark.sql.hive.HiveExternalMemoryCatalog"
+  private val HIVE_EXTERNAL_MEMORY_CATALOG_CLASS_NAME =
+    "org.apache.spark.sql.hive.HiveExternalMemoryCatalog"
 
   private def externalCatalogClassName(conf: SparkConf): String = {
     conf.get(CATALOG_IMPLEMENTATION) match {
       case "hive" =>
-        if (conf.get("spark.sql.hiveCatalog.memory", "false") == "true")
+        if (conf.get("spark.sql.hiveCatalog.memory", "false") == "true") {
           HIVE_EXTERNAL_MEMORY_CATALOG_CLASS_NAME
-        else HIVE_EXTERNAL_CATALOG_CLASS_NAME
+        } else {
+          HIVE_EXTERNAL_CATALOG_CLASS_NAME
+        }
       case "in-memory" => classOf[InMemoryCatalog].getCanonicalName
     }
   }
