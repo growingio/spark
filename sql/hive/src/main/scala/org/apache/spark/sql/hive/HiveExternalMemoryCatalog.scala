b708aa42163b52b87262bd5298c96907812619a0
guowei2 <guowei@growingio.com>
2020-06-05 16:48:40 +0800

    add database cache for hive catalog

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalMemoryCatalog.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalMemoryCatalog.scala
@@ -20,51 +20,71 @@ package org.apache.spark.sql.hive
 import java.util.concurrent.TimeUnit
 
 import scala.collection.mutable
-
 import org.apache.hadoop.conf.Configuration
-
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
-import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.catalyst.catalog.CatalogTypes.TablePartitionSpec
+import org.apache.spark.sql.catalyst.catalog.{
+  CatalogDatabase,
+  CatalogTable,
+  CatalogTablePartition
+}
 import org.apache.spark.util.ThreadUtils
 
-private[spark] class HiveExternalMemoryCatalog(conf: SparkConf, hadoopConf: Configuration)
-    extends HiveExternalCatalog(conf, hadoopConf) with Logging {
+private[spark] class HiveExternalMemoryCatalog(conf: SparkConf,
+                                               hadoopConf: Configuration)
+    extends HiveExternalCatalog(conf, hadoopConf)
+    with Logging {
 
-  val meta = new mutable.HashMap[String, CatalogTable]()
-  val refreshTime = conf.get("spark.sql.hiveCatalog.memory.refreshTime", "600").toInt
-  val executor = ThreadUtils.newDaemonSingleThreadScheduledExecutor("hive-memory-catalog-refresh")
+  val meta_db = new mutable.HashMap[String, CatalogDatabase]()
+  val meta_tbl = new mutable.HashMap[String, CatalogTable]()
+  val refreshTime =
+    conf.get("spark.sql.hiveCatalog.memory.refreshTime", "600").toInt
+  val executor = ThreadUtils.newDaemonSingleThreadScheduledExecutor(
+    "hive-memory-catalog-refresh"
+  )
 
   RefreshThreadStart()
 
   override def getTable(db: String, table: String): CatalogTable = {
-    meta.synchronized {
-      meta.getOrElseUpdate(s"db:$db-tbl:$table", {
+    meta_tbl.synchronized {
+      meta_tbl.getOrElseUpdate(s"db:$db-tbl:$table", {
         super.getTable(db, table)
       })
     }
   }
 
-  override def dropTable(
-      db: String,
-      table: String,
-      ignoreIfNotExists: Boolean,
-      purge: Boolean): Unit = {
+  override def dropTable(db: String,
+                         table: String,
+                         ignoreIfNotExists: Boolean,
+                         purge: Boolean): Unit = {
 
-    meta.synchronized {
-      meta.remove(s"db:$db-tbl:$table")
+    meta_tbl.synchronized {
+      meta_tbl.remove(s"db:$db-tbl:$table")
     }
     super.dropTable(db, table, ignoreIfNotExists, purge)
   }
 
+  override def getDatabase(db: String): CatalogDatabase = {
+    meta_db.synchronized {
+      meta_db.getOrElseUpdate(s"db:$db:$db", {
+        super.getDatabase(db)
+      })
+    }
+  }
+
+  override def getPartition(db: String,
+                            table: String,
+                            spec: TablePartitionSpec): CatalogTablePartition =
+    super.getPartition(db, table, spec)
+
   private def RefreshThreadStart(): Unit = {
-    executor.schedule(
-      new Runnable {
-        override def run(): Unit = meta.synchronized(meta.clear())
-      },
-      refreshTime,
-      TimeUnit.SECONDS
-    )
+    executor.schedule(new Runnable {
+      override def run(): Unit = {
+        meta_db.synchronized(meta_db.clear())
+        meta_tbl.synchronized(meta_tbl.clear())
+      }
+    }, refreshTime, TimeUnit.SECONDS)
   }
 
 }
